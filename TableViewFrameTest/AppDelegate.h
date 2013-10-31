59783ee586ef248062d35a67ea2b36ec6b3389c0
Ilja Iwas <ilja@iwascoding.com>
2013-10-31 16:08:04 +0100

    Initial Commit

---
+++ TableViewFrameTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewFrameTest
+//
+//  Created by ilja on 31.10.13.
+//  Copyright (c) 2013 iwascoding GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewFrameTestTests/TableViewFrameTestTests.m
@@ -0,0 +1,34 @@
+//
+//  TableViewFrameTestTests.m
+//  TableViewFrameTestTests
+//
+//  Created by ilja on 31.10.13.
+//  Copyright (c) 2013 iwascoding GmbH. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewFrameTestTests : XCTestCase
+
+@end
+
+@implementation TableViewFrameTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
